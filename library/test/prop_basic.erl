82595f2b940a791ec4f7485925e6c106cec7cda7
Miroslav Čech <Miroslav.Cech@ext.otpbank.hu>
2024-04-12 09:12:24 +0200

    Test basic properties of sort

--- library/test/prop_basic.erl
+++ library/test/prop_basic.erl
@@ -8,13 +8,20 @@ prop_test() ->
     ?FORALL(TupleList, tuples(),
         begin
             Result = library:sort_tuples(TupleList),
-            true
+            is_same_length(TupleList, Result)
+            andalso contains_same_elements(TupleList, Result)
         end).
 
 %%%%%%%%%%%%%%%
 %%% Helpers %%%
 %%%%%%%%%%%%%%%
-boolean(_) -> true.
+is_same_length(A, B) ->
+  length(A) =:= length(B).
+
+contains_same_elements(A, B) ->
+  SetA = sets:from_list(A),
+  SetB = sets:from_list(B),
+  sets:is_subset(SetA, SetB) andalso sets:is_subset(SetB, SetA).
 
 %%%%%%%%%%%%%%%%%%
 %%% Generators %%%
