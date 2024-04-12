1ace92fa8b8ec7954a7f5ed817716a3924b699ef
Miroslav Čech <Miroslav.Cech@ext.otpbank.hu>
2024-04-12 10:28:47 +0200

    Use automated symbolic calls

--- library/test/prop_symb.erl
+++ library/test/prop_symb.erl
@@ -1,11 +1,12 @@
 -module(prop_symb).
+-compile(export_all).
 -include_lib("proper/include/proper.hrl").
 
 %%%%%%%%%%%%%%%%%%
 %%% Properties %%%
 %%%%%%%%%%%%%%%%%%
 prop_test() ->
-    ?FORALL(D, dict(term()),
+    ?FORALL(D, dict_symbolic(),
         begin
             dict:size(D) < 5
         end).
@@ -15,3 +16,9 @@ prop_test() ->
 %%%%%%%%%%%%%%%%%%
 dict(T) ->
   ?LET(L, list({T, T}), dict:from_list(L)).
+
+dict_symbolic() ->
+  ?SIZED(Size, dict_symbolic(Size, {'$call', dict, new, []})).
+dict_symbolic(0, Dict) -> Dict;
+dict_symbolic(N, Dict) ->
+  dict_symbolic(N-1, {'$call', dict, store, [integer(), integer(), Dict]}).
